74ea0414e5ac0f6e97117d17a08a3d5b5d16a60d
toribro <86289260+toribro@users.noreply.github.com>
2024-07-06 19:13:39 +0900

    [Silver IV] Title: 카드 놓기, Time: 140 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.java
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -8,37 +8,40 @@ import java.util.HashSet;
 public class Main {
 	static int N,k=0;
 	static boolean[] visited;
-	static int[] cards;
+	static String[] cards;
 	static HashSet<String>list;
-	
+
 	public static void main(String[] args)throws IOException {
 		StreamTokenizer st= new StreamTokenizer(System.in);
 		st.nextToken();
 		N=(int)st.nval;
 		st.nextToken();
 		k=(int)st.nval;
-		cards=new int[N];
+		cards=new String[N];
 		visited=new boolean[N];
 		list=new HashSet<>();
 		int length=cards.length;
 		for(int i=0; i<length; i++) {
 			st.nextToken();
-			cards[i]=(int)st.nval;
+			cards[i]=(int)st.nval+"";
 		}
+		
 		dfs(0,"");
 		System.out.println(list.size());
+		
 	}
 	
 	private static void dfs(int count,String temp) {
 		
 		if(count==k) {
-			list.add(temp);	
-            return;
+			list.add(temp);
+			return;
 		}
+		
 		for(int i=0; i<N; i++) {
 			if(!visited[i]) {
 				visited[i]=true;
-				dfs(count+1,temp+(String.valueOf(cards[i])));
+				dfs(count+1,temp+(cards[i]));
 				visited[i]=false;
 			}
 		}
