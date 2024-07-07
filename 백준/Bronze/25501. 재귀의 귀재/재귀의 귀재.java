687d4d8f36f93aa3dbe5d75c6af109ef7d3fbe9d
toribro <86289260+toribro@users.noreply.github.com>
2024-07-07 21:34:33 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 232 ms, Memory: 19736 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -1,10 +1,8 @@
-import java.io.StreamTokenizer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -14,32 +12,31 @@ public class Main {
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter output =new BufferedWriter(new OutputStreamWriter(System.out));
 		int N =Integer.parseInt(input.readLine());
-		
-		String[] words= new String[N];
+	
+		StringBuilder words =new StringBuilder();
 		for(int i=0; i<N; i++) {
-			words[i]=input.readLine();
-		}
-		
-		for(int i=0; i<N; i++) {
-			output.write(isPalindrome(words[i].toCharArray())+" ");
-			output.write(String.valueOf(count));
-			output.newLine();
-			output.flush();
+			
+			words.append(isPalindrome(input.readLine())).append(" ");
+			words.append(count).append("\n");
 			count=0;
 		}
+	
+		output.write(words.toString());
 		input.close();
 		output.close();
+		
 	}
 	
-	private static int isPalindrome(char[]words) {
-		return recursion(words,0,words.length-1);
+	private static int isPalindrome(String words) {
+		return recursion(words,0,words.length()-1);
 	}
 	
-	private static int recursion(char[] words,int l,int r) {
+	private static int recursion(String words,int l,int r) {
 		count++;
 		if(l>=r) {
 			return 1;
-		}else if(words[l]!=words[r]) return 0;
+		}else if(words.charAt(l)!=words.charAt(r)) return 0;
 		else return recursion(words,l+1,r-1);
 	}
+
 }
