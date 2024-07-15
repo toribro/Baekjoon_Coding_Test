d43b1ea9eac9988a5500a60cb068a4a8bad46176
toribro <86289260+toribro@users.noreply.github.com>
2024-07-15 11:57:40 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,44 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+
+public class Main {
+	private static int N;
+
+	public static void main(String[] args)throws IOException {
+		StreamTokenizer input =new StreamTokenizer(System.in);
+		BufferedWriter output=new BufferedWriter(new OutputStreamWriter(System.out));
+
+		input.nextToken();
+		N=(int)input.nval;
+        
+		output.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다."+"\n");
+		output.flush();
+		recursive(output,0);
+		output.flush();
+		output.close();
+	}
+	
+	
+	private static void recursive(BufferedWriter output,int i) throws IOException {
+		StringBuilder word=new StringBuilder();
+		for(int k=0; k<i; k++) {
+			word.append("____");
+		}
+		
+		output.write(word+"\"재귀함수가 뭔가요?\"\n");
+		
+		if(i==N) {
+			output.write(word+"\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+		}else {
+			 output.write(word + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+	         output.write(word + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+	         output.write(word + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+	         recursive(output, i + 1);
+		}
+		output.write(word+"라고 답변하였지.\n");
+		
+	}
+	
+}
